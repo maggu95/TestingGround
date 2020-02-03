1b092d33cea5e111c42091dad5cf12ec0cf1b2b0
maggu95 <57000576+maggu95@users.noreply.github.com>
2020-02-03 21:36:41 +0100

    laget filstrukturen og lastet inn resurser

---
+++ index.js
@@ -0,0 +1,16 @@
+class App extends React.Component {
+  // reference to both the video and canvas
+  videoRef = React.createRef();
+  canvasRef = React.createRef();
+
+  // we are gonna use inline style
+  styles = {
+    position: 'fixed',
+    top: 150,
+    left: 150,
+  };
+  
+  detectFromVideoFrame = (model, video) => {
+    
+  };
+}
